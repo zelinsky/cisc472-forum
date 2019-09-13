2c6a201308d926f9a1f441830414bb168de18490
Matthew Zelinsky <zelinsky@udel.edu>
2019-09-13 09:57:37 -0400

    Added html and javascript to display posts.

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 
-
+    const showPosts = function(posts) {
+	const postDisplay = $("#posts");
+	postDisplay.html('');
+	posts.forEach( p => {
+	    postDisplay.append(`<h3 id="post_id">Post ${p.id}></h3><br><p id="post_content">${p.content}</p><br><p id="post_author">${p.author}</p><br><time id="post_timestamp">${p.timestamp}</time>`);
+	});
+    }
+    
+    $("#get_posts_btn").click( () => {
+	$.ajax({
+	    method: "GET",
+	    url: "api.php",
+	    success: data=>{showPosts(data)}
+	});
+	console.log("yo");
+    });
 
 });
